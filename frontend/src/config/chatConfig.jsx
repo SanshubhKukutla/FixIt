65202d93b401f2a55788652867b3d96c7a7735da
RoyceAroc <royceaden@gmail.com>
2024-10-27 01:01:21 -0400

    remove frontend

--- frontend/src/config/chatConfig.jsx
+++
@@ -1,8 +0,0 @@
-export const chatConfig = {
-  botUrl: 'https://bots.dev.studio.nlx.ai/c/RqzCCVzAlj9QasR6311qi/SyvNbE69-QYeWAMhnY0oS',
-  headers: {
-    'nlx-api-key': 'IFCR227A/pEUWdztaA',
-    'Content-Type': 'application/json'
-  },
-  languageCode: 'en-US'
-};
